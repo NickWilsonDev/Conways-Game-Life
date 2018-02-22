af70e98f76d7e61d31bab97d296e6254848d1188
Nick Wilson <logikfx@gmail.com>
2018-02-21 23:37:25 -0500

    Added basic index html file and started the main JS file.

---
+++ main.js
@@ -0,0 +1,10 @@
+/* main.js */
+
+/*
+    Rules
+    1. Any live cell with fewer than two live neighbors dies.
+    2. Any live cell with two or three live neighbors lives on to the next generation.
+    3. Any live cell with more than three neighbors dies, as if overpopulation.
+    4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+*/
+
